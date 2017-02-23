5ada0957103c1912e0a019f4971a29af76a898e1
João Fernando <joaofernando.costa@corp.globo.com>
2017-02-23 11:19:46 -0300

    Fix tests by correcting typo 'expext' to 'expect'

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -1,4 +1,4 @@
-var expext = require('expect');
+var expect = require('expect');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
